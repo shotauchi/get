7eec3dd2b2e021f4cc66f9453baebfb9d145af3e
[shotauchi] <[tianneixiang84@gmail.com]>
2025-03-13 18:49:35 +0000

    javascript-03~04

---
+++ javascript-03/3-11/3-11.js
@@ -0,0 +1,11 @@
+let mon = 'Octover';
+
+if (mon > 2) {
+    console.log('monは2より大きいです。')    
+} else if (mon < 4) {
+    console.log('monは4より小さいです。')
+} else if (mon >= 6) {
+    console.log('monは6より大きいです。')
+}else {
+    console.log('monは10月です。')
+}
